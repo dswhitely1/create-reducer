9af82b24b0110e8b891f2d1fab72d2c2ac29a110
Donald Whitely <dswhitely1@gmail.com>
2020-05-16 22:36:35 -0400

    Added Eslint and prettier

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,14 @@
+interface Action<T = any> {
+    type: T;
+}
+interface AnyAction extends Action {
+    [extraProps: string]: any;
+}
+declare type Reducer<S = any, A extends Action = AnyAction> = (state: S | undefined, action: A) => S;
 declare type State = any;
 declare type FunctionMap = {
     [key: string]: (state: State, payload: any) => void;
 };
-declare type Action = {
-    type: string;
-    payload: any;
-};
-declare type CreateReducer<S = State, F = FunctionMap> = (initialState: S, fnMap: F) => (state: S, action: Action) => S;
+declare type CreateReducer<S = State, F = FunctionMap> = (initialState: S, fnMap: F) => Reducer;
 export declare const createReducer: CreateReducer;
 export {};
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,34 @@
-type State = any
+/*
+    Action, AnyAction, and Reducer are from the Redux Library
+ */
+
+interface Action<T = any> {
+  type: T;
+}
+
+interface AnyAction extends Action {
+  [extraProps: string]: any;
+}
+
+type Reducer<S = any, A extends Action = AnyAction> = (
+  state: S | undefined,
+  action: A
+) => S;
+
+type State = any;
 type FunctionMap = {
-    [key: string]: (state: State, payload: any) => void
-}
-type Action = {
-    type: string
-    payload: any
-}
+  [key: string]: (state: State, payload: any) => void;
+};
 
-type CreateReducer<S = State, F = FunctionMap> = (initialState: S, fnMap: F) => (state: S, action: Action) => S
+type CreateReducer<S = State, F = FunctionMap> = (
+  initialState: S,
+  fnMap: F
+) => Reducer;
 
-export const createReducer:CreateReducer = (initialState, fnMap) => (state = initialState, {type, payload}) => {
-    const handler = fnMap[type]
-    return handler ? handler(state, payload) : state;
-}
+export const createReducer: CreateReducer = (initialState, fnMap) => (
+  state = initialState,
+  { type, payload }
+) => {
+  const handler = fnMap[type];
+  return handler ? handler(state, payload) : state;
+};
